5bac7b5cd9b69694096513b9f92aa74e5811bf85
Zhu-Peter <gzhu@ualberta.ca>
2024-07-29 19:33:49 -0600

    added some procedures

--- queries.sql
+++ queries.sql
@@ -47,10 +47,10 @@ create table restaurant (
     city varchar(255) not null,
     profile_url varchar(255) not null,
     banner_url varchar(255) not null,
-    restaurant_id int not null auto_increment,
+    id int not null auto_increment,
     email varchar(255) not null,
     password varchar(255) not null,
-    primary key (restaurant_id)
+    primary key (id)
 );
 
 -- restaurant_session
@@ -68,13 +68,15 @@ create table restaurant_session (
         -- price: (number)
 create table menu_item (
     id int not null auto_increment,
+    restaurant_id int not null,
     name varchar(255) not null,
     description varchar(255) not null,
     price decimal(10,2) not null,
     image_url varchar(255) not null,
-    primary key (id)
+    primary key (id),
+    foreign key (restaurant_id) references restaurant(id)
 
-    
+);
 
 -- order
 create table order (
@@ -86,7 +88,7 @@ create table order (
     foreign key (restaurant_id) references restaurant(id),
     foreign key (client_id) references client(id),
     primary key (id)
-)
+);
 
 -- order_menu_item
 create table order_menu_item (
@@ -98,3 +100,40 @@ create table order_menu_item (
     foreign key (menu_item_id) references menu_item(id),
     primary key (id)
 );
+
+create procedure get_client(client_id int)
+    select * from client where id = client_id;
+
+create procedure get_restaurant(restaurant_id int)
+    select * from restaurant where id = restaurant_id;
+
+create procedure get_restaurants
+    select * from restaurant;
+
+create procedure get_menu_item(restaurant int)
+    select * from menu_item where restaurant_id = restaurant;
+
+create procedure client_login(username_input varchar(255), password_input varchar(255))
+    select id from client where username = username_input and password = password_input;
+
+create procedure restaurant_login(email_input varchar(255), password_input varchar(255))
+    select id from restaurant where email = email_input and password = password_input;
+
+DELIMITER $$
+$$
+create DEFINER=`root`@`localhost` procedure `get_client_orders`(token_input varchar(255))
+begin
+    DECLARE token_id int;
+    select id into token_id from restaurant_session where token = token_input;
+
+    IF token_id IS NULL THEN
+
+        ROLLBACK;
+
+        SELECT 'Invalid token' AS message;
+
+    ELSE
+
+    select * from order_item where restaurant_id = token_id;
+end$$
+DELIMITER ;
